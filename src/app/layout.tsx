ac6e2ff8af5b2c941d484e545c3d9b7c0f526ed0
lucy bissong <lucybissong@gmail.com>
2026-06-07 19:42:25 +0100

    Create layout.tsx

---
+++ src/app/layout.tsx
@@ -0,0 +1,13 @@
+```tsx id="wjlwm2"
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
+```
