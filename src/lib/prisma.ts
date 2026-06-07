c6ad1df5a827bbd60ff7c450ba81dbb751b03153
lucy bissong <lucybissong@gmail.com>
2026-06-07 23:44:46 +0100

    Update prisma.ts

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -4,10 +4,12 @@ const globalForPrisma = global as unknown as {
   prisma: PrismaClient;
 };
 
-export const prisma =
+const prisma =
   globalForPrisma.prisma ||
   new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
 }
+
+export default prisma;
